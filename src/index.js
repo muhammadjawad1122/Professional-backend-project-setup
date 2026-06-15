289d00dd4c847e11cec3513a1faf2a939187e187
muhammadjawad1122 <ishaqkhan12345ab@gmail.com>
2026-06-15 23:55:59 +0500

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 /*require(`dotenv`).config({path:`/.env`})*/
 import dns from "node:dns";
-
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-import dotenv from 'dotenv'
 
-
-import connectDB from './db/index.js'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'/.env'
-})
+  path: "./.env"
+});
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT||8000,(()=>{
-     console.log(`server is running at PORT : {process.env.PORT}`);
-     
-    }));
-    
-})
-.catch((err){
-    console.log("MongoDB connection failed !1!", err);
-    
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is running at PORT : ${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!", err);
+  });
 
 
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,46 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from 'fs'
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: rpocess.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+
+    const uplodeOnCloudinary=async (localFilePath)=>{
+        try {
+            if(!localFilePath) return null
+            // uplode the file on cloudinary
+            const response=await cloudinary.uploder.upload(localFilePath,{
+                resource_type:"auto"
+            })
+            //file has been successfully uplode
+            console.log("file is uploded on cloudinary",response.url);
+            return response;
+            
+        } catch (error) {
+            fs.unlinkSync(localFilePath) // remove the locally saved temporary
+        // file as the uplode operation got failed
+        return null;
+            
+        }
+    }
+
+
+        // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+
+
+    export {uplodeOnCloudinary}
